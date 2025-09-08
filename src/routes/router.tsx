9301deb92f553d281f9406e7926f3006d01e5a4a
Junsub Lee <lee980605@knu.ac.kr>
2025-09-09 02:34:07 +0900

    feat: 기본 Router 및 경로 상수 추가
    ROUTE_PATH 상수를 정의하고 추후 라우팅을 고려했습니다.

---
+++ src/routes/router.tsx
@@ -0,0 +1,18 @@
+import { Routes } from "react-router-dom";
+//import { ROUTE_PATH } from "./path";
+
+//import OnboardingPage from "@/pages/OnboardingPage";
+
+const Router = () => {
+  return (
+    <Routes>
+      {/* 온보딩 (앱 진입 시 첫 화면) */}
+
+      {/* 사용자 영역 */}
+
+      {/* 오너 영역 */}
+    </Routes>
+  );
+};
+
+export default Router;
