6a67abd47654acaa3594f59c3078aa39e95ff7c6
Junsub Lee <lee980605@knu.ac.kr>
2025-09-09 03:10:23 +0900

    feat: Router에 온보딩 페이지 경로 추가
    routes/path.ts에 ROUTE_PATH.MAIN('/')를 정의하고 Router.tsx에 OnboardingPage를 기본 라우트로 등록했습니다.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,13 +1,14 @@
-import { Routes } from "react-router-dom";
-//import { ROUTE_PATH } from "./path";
+import { Routes, Route } from "react-router-dom";
+// If the correct path is "@/routes/path", update as follows:
+import { ROUTE_PATH } from "@/routes/paths";
 
-//import OnboardingPage from "@/pages/OnboardingPage";
+import OnboardingPage from "@/pages/OnboardingPage";
 
 const Router = () => {
   return (
     <Routes>
       {/* 온보딩 (앱 진입 시 첫 화면) */}
-
+      <Route path={ROUTE_PATH.MAIN} element={<OnboardingPage />} />
       {/* 사용자 영역 */}
 
       {/* 오너 영역 */}
