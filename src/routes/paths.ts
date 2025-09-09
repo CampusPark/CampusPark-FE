836850ea0b955ffe414cb545b39030332f9568e2
Junsub Lee <lee980605@knu.ac.kr>
2025-09-09 23:36:06 +0900

    feat: Router 추가 -> 하단 네비게이션에 필요한 paths 미리 추가

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -1,3 +1,7 @@
 export const ROUTE_PATH = {
-  MAIN: "/",
+  ONBOARDING: "/", // 시작 화면
+  HOME: "/home", // 지도/메인 페이지
+  RESERVATIONS: "/reservations", // 예약 내역
+  REGISTER: "/register", // 공간 등록
+  MYPAGE: "/mypage", // 마이 페이지
 };
