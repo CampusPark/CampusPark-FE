2e4592e82b3fe8f74badd6e8c6476540605639c7
mejin <wls4482@gmail.com>
2025-09-10 05:03:53 +0900

    feat: 라우트 설정, register에 대한 경로 변경

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,10 +3,10 @@ import { ROUTE_PATH } from "@/routes/paths";
 
 import OnboardingPage from "@/pages/OnboardingPage";
 import MonitorPage from "@/pages/owner/MonitorPage";
+import SpacesPage from "@/pages/owner/SpacesPage";
 import HomePage from "@/pages/HomePage";
 // 추후 생성될 페이지 더미
 const ReservationsPage = () => <div>예약 내역 페이지</div>;
-const RegisterPage = () => <div>공간 등록 페이지</div>;
 const MyPage = () => <div>마이 페이지</div>;
 
 export default function AppRouter() {
@@ -15,7 +15,7 @@ export default function AppRouter() {
       <Route path={ROUTE_PATH.ONBOARDING} element={<OnboardingPage />} />
       <Route path={ROUTE_PATH.HOME} element={<HomePage />} />
       <Route path={ROUTE_PATH.RESERVATIONS} element={<ReservationsPage />} />
-      <Route path={ROUTE_PATH.REGISTER} element={<RegisterPage />} />
+      <Route path={ROUTE_PATH.REGISTER} element={<SpacesPage />} />
       <Route path={ROUTE_PATH.MYPAGE} element={<MyPage />} />
       <Route path={ROUTE_PATH.MONITOR} element={<MonitorPage />} />
     </Routes>
