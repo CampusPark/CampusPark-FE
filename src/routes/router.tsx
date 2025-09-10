75624539bfd407839c37b806763a933612a1ea64
Junsub Lee <lee980605@knu.ac.kr>
2025-09-11 02:21:59 +0900

    feat: 검색 페이지 및 상세 검색 페이지 경로 추가

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,6 +3,9 @@ import { ROUTE_PATH } from "@/routes/paths";
 
 import OnboardingPage from "@/pages/OnboardingPage";
 import HomePage from "@/pages/HomePage";
+import SearchPage from "@/pages/search/SearchPage";
+import SearchDetailPage from "@/pages/search/SearchDetailPage";
+
 // 추후 생성될 페이지 더미
 const ReservationsPage = () => <div>예약 내역 페이지</div>;
 const RegisterPage = () => <div>공간 등록 페이지</div>;
@@ -13,6 +16,9 @@ export default function AppRouter() {
     <Routes>
       <Route path={ROUTE_PATH.ONBOARDING} element={<OnboardingPage />} />
       <Route path={ROUTE_PATH.HOME} element={<HomePage />} />
+      <Route path={ROUTE_PATH.SEARCH} element={<SearchPage />} />
+      <Route path={ROUTE_PATH.SEARCH_DETAIL} element={<SearchDetailPage />} />
+
       <Route path={ROUTE_PATH.RESERVATIONS} element={<ReservationsPage />} />
       <Route path={ROUTE_PATH.REGISTER} element={<RegisterPage />} />
       <Route path={ROUTE_PATH.MYPAGE} element={<MyPage />} />
